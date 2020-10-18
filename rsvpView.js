4f8963b471ba6f2b814d29638bbc23ef585da564
eternalstillness <60407017+eternalstillness@users.noreply.github.com>
2020-10-18 11:06:46 -0700

    adding .js files to rsvp form

---
+++ rsvpView.js
@@ -0,0 +1,9 @@
+function changeSize(){
+    if(this.checked == true){
+        // increase the size of the userInputBox and show the other two elements
+        // setTimeout()
+    }
+    else if (this.checked != true){
+        // decrease the size of the userInputBox and hide the two other elements
+    }
+}
