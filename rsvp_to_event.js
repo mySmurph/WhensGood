92c0ea106238ac6658dc81b08ffeae95e0784c96
mySmurph <56486787+mySmurph@users.noreply.github.com>
2020-10-27 09:21:41 -0700

    RSVP to Event page <!-- T.V. PASS! --> Changes to switch

--- rsvp_to_event.js
+++ rsvp_to_event.js
@@ -2,10 +2,11 @@
 let domChooseFile = document.getElementById("choose_file");
 let domName = document.getElementById("name");
 let domEmail = document.getElementById("email");
+let domSlider = document.getElementById("switchbox");
 
 domChooseFile.setAttribute("placeholder", "Choose File...");
 domName.setAttribute("placeholder", "Zapp Brannigan");
 domEmail.setAttribute("placeholder", "example@email.domain"); 
 
-domSlider.addEventListener("click", changeSize, false);
-domBody.addEventListener("load", elements_visibility, false);
+// domSlider.addEventListener("click", changeSize, false);
+// domBody.addEventListener("load", elements_visibility, false);
--- rsvp_view.js
+++ rsvp_view.js
@@ -3,13 +3,13 @@ let domBody = document.getElementById("body");
 let domUserInputContainer = document.getElementById("userInputContainer");
 let domPasswordText = document.getElementById("password_text");
 let domInputPassword = document.getElementById("password_input");
-let domSlider = document.getElementById("slider");
+let domSlider = document.getElementById("switchbox");
 
 domEventCode.setAttribute("placeholder", "2F088152DE");
 domInputPassword.setAttribute("placeholder", "password");
 
-document.getElementById('slider').addEventListener("change", changeSize);
-var user_type = document.getElementById('slider');
+document.getElementById('switchbox').addEventListener("change", changeSize);
+var user_type = document.getElementById('switchbox');
 function changeSize(){
 	var password = document.getElementById("password_container");
     if(user_type.checked){
