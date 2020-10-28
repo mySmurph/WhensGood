745efa4ce2a28a15cba001397bb0ababe228037b
mySmurph <56486787+mySmurph@users.noreply.github.com>
2020-10-27 19:04:42 -0700

    senter sheduel event validation

---
+++ script/enter_schedule_event.js
@@ -0,0 +1,16 @@
+document.getElementById("button").addEventListener("click", validateCridentials);
+var domEventCode = document.getElementById("event_code");
+var domInputPassword = document.getElementById("password_input");
+
+domEventCode.setAttribute("placeholder", "2F088152DE");
+domInputPassword.setAttribute("placeholder", "password");
+
+function validateCridentials(){
+	var eventCode = domEventCode.value;
+	var password = domInputPassword.value;
+	if(password==0 || eventCode==0){
+		alert("no event found");
+		// window.location.replace("landing.html"); //redirect to home
+		// return false;
+	}
+}
