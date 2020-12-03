c0c9795ee4e8f4289cd500b7c2bca69de47b71a0
mySmurph <56486787+mySmurph@users.noreply.github.com>
2020-12-02 18:00:41 -0800

    php connect ot db function

--- functions.php
+++ functions.php
@@ -5,4 +5,21 @@
 		echo fread($myfile,filesize($nav));
 		fclose($myfile);
 	};
+
+	function connectDB(){
+		$host =  'localhost';	//cis444 server
+		// $host =  'db';	// Local Server
+		$userid =  'group4';
+		$password = 'IjChbKtynlNZ';
+		$schema =  'group4';
+
+		$db = new mysqli($host, $userid,  $password, $schema);
+
+		if ($db->connect_error){
+			print "<p class = \"dbStatus\">Unable to Connect to MySQL</p>". $db -> connect_error;
+			exit;
+		}else{
+			return $db;
+		}
+	};
 ?>
